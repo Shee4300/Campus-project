d4d388638d361a4768b15bfb6efe51efb37b5422
Shee4300 <shrivastavsheetal82@gmail.com>
2026-09-02 01:26:21 +0530

    Prepare project for deployment

--- app.js
+++ app.js
@@ -252,6 +252,8 @@ app.get("/contact", (req, res) => {
     res.render("contact");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
